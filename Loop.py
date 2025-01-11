cdd74f6ea7332a4118df7cfa2f0fa01b5ea58230
kanadi23 <kanadindijah1@gmail.com>
2025-01-11 13:55:14 +0100

    Add files via upload

---
+++ Loop.py
@@ -0,0 +1,14 @@
+# # LOOPS
+# # WHILE
+# x = 50
+# y = 1
+
+# while y < 50:
+#     print("Welcome")
+
+x = 10
+y = 1
+
+while y <x:
+    print("Welcome")
+    y += 1
---
+++ exercise.py
@@ -0,0 +1,18 @@
+names = ["mohammed", "siyama", "faruq,"]
+# print(dir(name))
+new_names = ["lawal", "haruna"]
+
+ADDING DATA.
+names.append("maryam")
+names,append("True")
+
+ADDING MULTIPLE DATA
+names.extend(new_names)
+names.extend("shegun")
+names.append(new_names)
+
+INSERTION IN A POSITION
+names.insert(2, )
+
+
+print(names)
